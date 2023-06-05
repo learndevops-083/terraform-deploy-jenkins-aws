d9fd403af17f318ea467d2f243ff00cde17eb334
learndevops-083 <113036991+learndevops-083@users.noreply.github.com>
2023-06-05 01:48:12 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,14 @@
-variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
+variable "vpc_cidr_block" {
+  description = "VPC CIDR Block"
+  type = string 
+  default = "10.0.0.0/16"
+}
+variable "subnet_cidr_block" {
+  type = string 
+  default = "10.0.1.0/24"
+}
 
-variable "avail_zone" {
+variable "availability_zone" {
   type = string
   default = "us-east-1a" 
 }
