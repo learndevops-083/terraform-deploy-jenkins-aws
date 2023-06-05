ab15a593fb89880f952746bb175f098eee3cefcc
learndevops-083 <113036991+learndevops-083@users.noreply.github.com>
2023-06-05 02:08:16 +0100

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -47,6 +47,13 @@ resource "aws_default_security_group" "default-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "Allow all ingress traffic"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
