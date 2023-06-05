b779d2ca18c6951e7bbe484db8931dc6d8d0d06e
learndevops-083 <113036991+learndevops-083@users.noreply.github.com>
2023-06-05 01:38:30 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,29 @@
 variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
-variable "avail_zone" {}
-variable "aws_region" {}
-variable "env_prefix" {}
-variable "instance_type" {}
-variable "instance_keypair" {}
+
+variable "avail_zone" {
+  type = string
+  default = "us-east-1a" 
+}
+
+variable "aws_region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "env_prefix" {
+  type = string
+  default = "staging"
+  
+}
+
+variable "instance_type" {
+   description = "EC2 Instance Type"
+   type = string
+   default = "t2.small"
+}
+variable "instance_keypair" {
+  description = "AWS EC2 Key Pair that need to be associated with EC2 Instance"
+  type = string
+  default = "kp"
+}
