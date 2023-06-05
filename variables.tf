6023f47012baa8744bbdefb1c514acc1d9a5388f
learndevops-083 <113036991+learndevops-083@users.noreply.github.com>
2023-06-05 01:07:19 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
 variable "avail_zone" {}
+variable "aws_region" {}
 variable "env_prefix" {}
 variable "instance_type" {}
 variable "instance_keypair" {}
