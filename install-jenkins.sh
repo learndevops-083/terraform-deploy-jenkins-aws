b2035880bbff2510ce46af15fab7a20d2af095a5
learndevops-083 <113036991+learndevops-083@users.noreply.github.com>
2023-06-05 02:21:45 +0100

    Update install-jenkins.sh

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -16,7 +16,7 @@ sudo yum install git -y
 # install jenkins
 sudo wget -O /etc/yum.repos.d/jenkins.repo
 https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
 sudo yum install jenkins -y
 sudo systemctl enable jenkins
